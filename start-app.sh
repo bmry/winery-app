9a9cc33f19c79d12985db61e490824d214e9e297
Morayo <mbamgbose@accounteer.com>
2019-02-26 19:55:18 +0100

    command updated

--- start-app.sh
+++ start-app.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-php bin/console server:run
+echo "Creating Database schema.."
 php bin/console doctrine:schema:update --force
-php bin/console app:load_rss_feed
-open "http://localhost:8000
+php bin/console server:run
+
+echo "Getting Wine Inventory..."
+php bin/console app:load_rss_feed &
+echo "Clearing cache"
+php bin/console cache:clear --no-warmup --env=prod
+
+open http://localhost:8000
