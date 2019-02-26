a3c359d3e548618ccb7d342570498bd63d7e92b1
Morayo <mbamgbose@accounteer.com>
2019-02-26 20:04:51 +0100

    start commands updated

--- start-app.sh
+++ start-app.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+ping -n 11 127.0.0.1 > nul
+echo 'Creating Queue Exchange'
+php bin/console rabbitmq:setup-fabric
+php bin/console rabbitmq:consumer order_create_response &
+php bin/console rabbitmq:consumer order_create_request &
+php bin/console rabbitmq:consumer wine_update &
+
 echo "Creating Database schema.."
 php bin/console doctrine:schema:update --force
-php bin/console server:run
-
 echo "Getting Wine Inventory..."
 php bin/console app:load_rss_feed &
 echo "Clearing cache"
 php bin/console cache:clear --no-warmup --env=prod
+php bin/console server:run
 
 open http://localhost:8000
--- start-rabbit.sh
+++ start-rabbit.sh
@@ -1,8 +1,2 @@
 docker build --tag rabbitmq -< rabbitmq/Dockerfile
 docker run -d --hostname winery --name winery -p 5674:5672 -p 8086:15672 rabbitmq:3.7.4
-ping -n 11 127.0.0.1 > nul
-php bin/console rabbitmq:setup-fabric
-php bin/console rabbitmq:consumer order_create_response &
-php bin/console rabbitmq:consumer order_create_request &
-php bin/console rabbitmq:consumer wine_update &
-exit
