6284fec68981f6e71b0fd84e0c305166c5283240
Morayo <mbamgbose@accounteer.com>
2019-02-27 13:26:01 +0100

    start-app.bat updated

--- start-app.bat
+++ start-app.bat
@@ -1,10 +1,25 @@
+@ECHO OFF
+
 ping -n 11 127.0.0.1 > nul
+echo Setting Up Queue
 php bin/console rabbitmq:setup-fabric
-start cmd.exe @cmd /k "php bin/console rabbitmq:consumer order_create_response"
-start cmd.exe @cmd /k "php bin/console rabbitmq:consumer order_create_request"
-start cmd.exe @cmd /k "php bin/console rabbitmq:consumer wine_update"
-start cmd.exe @cmd /k "php bin/console doctrine:schema:update --force"
-start cmd.exe @cmd /k "php bin/console server:run"
-start cmd.exe @cmd /k "php bin/console app:load_rss_feed"
+echo Setting Up  Consumers
+start /b php bin/console rabbitmq:consumer order_create_response
+ping -n 11 127.0.0.1 > nul
+start /b php bin/console rabbitmq:consumer order_create_request
+ping -n 11 127.0.0.1 > nul
+start /b php bin/console rabbitmq:consumer wine_update
+
+echo Creating Database
+php bin/console doctrine:schema:update --force
+
+echo Loading wines into database
+php bin/console app:load_rss_feed
+
+echo Clearing cache
 php bin/console cache:clear --no-warmup --env=prod
+
+echo Starting application
+php bin/console server:run
+
 start http://localhost:8000
